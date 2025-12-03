3a9ec9045c5ef3971549fad1e6da3fb98f652d06
yusuf <yosa12978@gmail.com>
2025-12-04 01:05:56 +0300

    added concurrency

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"log/slog"
 	"os"
-	"slices"
 	"strconv"
 	"time"
 )
@@ -22,45 +21,43 @@ func main() {
 
 	ctx := context.TODO()
 	fetcher := NewDefaultFetcher(UserAgent, timeout)
-	links := NewQueue[string]()
-	links.Enqueue(initURLs...)
+
+	linksCh := make(chan []string)
+	go func() { linksCh <- initURLs }()
 
 	// getting website domains to follow
-	baseURLs := []string{}
-	for _, v := range initURLs {
-		url, err := BaseURL(v)
-		if err != nil {
-			panic(err)
-		}
-		baseURLs = append(baseURLs, url)
-	}
+	//baseURLs := []string{}
+	//for _, v := range initURLs {
+	//url, err := BaseURL(v)
+	//if err != nil {
+	//panic(err)
+	//}
+	//baseURLs = append(baseURLs, url)
+	//}
+	for links := range linksCh {
+		for _, link := range links {
+			//baseURL, _ := BaseURL(link)
+			//if !slices.Contains(baseURLs, baseURL) {
+			//continue
+			//}
 
-	for !links.Empty() {
-		url, err := links.Dequeue()
-		if err != nil {
-			continue
+			if fetched[link] {
+				continue
+			}
+			fetched[link] = true
+			go func() {
+				fetchedLinks, code, err := fetcher.Fetch(ctx, link)
+				if err != nil {
+					slog.Error("error while fetching",
+						"err", err.Error(),
+						"code", code,
+						"url", link,
+					)
+					return
+				}
+				slog.Info("fetch", "url", link, "code", code)
+				linksCh <- fetchedLinks
+			}()
 		}
-
-		baseURL, _ := BaseURL(url)
-		if !slices.Contains(baseURLs, baseURL) {
-			continue
-		}
-
-		if fetched[url] {
-			continue
-		}
-		fetched[url] = true
-
-		fetchedLinks, code, err := fetcher.Fetch(ctx, url)
-		if err != nil {
-			slog.Error("error while fetching",
-				"err", err.Error(),
-				"code", code,
-				"url", url,
-			)
-			continue
-		}
-		slog.Info("fetch", "url", url, "code", code)
-		links.Enqueue(fetchedLinks...)
 	}
 }
