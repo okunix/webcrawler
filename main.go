823b84498aae6cd873eb4cc9bef93eb157a05880
yusuf <yosa12978@gmail.com>
2025-12-04 13:14:50 +0300

    improved concurrency

--- main.go
+++ main.go
@@ -4,62 +4,30 @@ import (
 	"context"
 	"log/slog"
 	"os"
-	"slices"
 	"strconv"
-	"sync"
 	"time"
 )
 
 func main() {
-	initURLs := os.Args[1:]
-	// getting website domains to follow
-	baseURLs := []string{}
-	for _, v := range initURLs {
-		url, err := BaseURL(v)
-		if err != nil {
-			panic(err)
-		}
-		baseURLs = append(baseURLs, url)
-	}
-
-	fetched := make(map[string]bool)
-
+	// initializing fetcher
 	timeoutSec, err := strconv.Atoi(Timeout)
 	if err != nil {
 		panic(err)
 	}
 	timeout := time.Duration(timeoutSec) * time.Second
-
 	ctx := context.TODO()
 	fetcher := NewDefaultFetcher(UserAgent, timeout)
 
-	var n int
+	fetched := make(map[string]bool)
 	linksCh := make(chan []string)
-	go func() { linksCh <- initURLs }()
+	unseenLinkCh := make(chan string)
+	// loading urls from os.Args
+	go func() { linksCh <- os.Args[1:] }()
 
-	sem := make(chan struct{}, 20)
-	var wg sync.WaitGroup
-	n++
-	for ; n > 0; n-- {
-		links := <-linksCh
-		for _, link := range links {
-			baseURL, _ := BaseURL(link)
-			if !slices.Contains(baseURLs, baseURL) {
-				continue
-			}
-
-			if fetched[link] {
-				continue
-			}
-			fetched[link] = true
-			sem <- struct{}{}
-			wg.Add(1)
-			n++
-			go func() {
-				defer func() {
-					<-sem
-					wg.Done()
-				}()
+	// starting 20 workers
+	for range 20 {
+		go func() {
+			for link := range unseenLinkCh {
 				fetchedLinks, code, err := fetcher.Fetch(ctx, link)
 				if err != nil {
 					slog.Error("error while fetching",
@@ -70,9 +38,20 @@ func main() {
 					return
 				}
 				slog.Info("fetch", "url", link, "code", code)
-				linksCh <- fetchedLinks
-			}()
+				go func() { linksCh <- fetchedLinks }()
+			}
+		}()
+	}
+
+	// checking if we have seen link
+	// if not add it to unseenLink channel and mark as seen
+	for links := range linksCh {
+		for _, link := range links {
+			if fetched[link] {
+				continue
+			}
+			fetched[link] = true
+			unseenLinkCh <- link
 		}
 	}
-	wg.Wait()
 }
